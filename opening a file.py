54425f69bb7e6e93f25d0211efddd5889f9e9c5d
susmithauddaraju <103415205+susmithauddaraju@users.noreply.github.com>
2022-04-11 14:03:32 +0530

    Add files via upload

---
+++ opening a file.py
@@ -0,0 +1,67 @@
+"""f = open("teacherfulldetails.txt","r")
+print(type(f))"""
+
+"""print(True > False)
+print(True < False)"""
+
+"""john = 3
+mary = 5
+adam = 6
+print('john :' , john)"""
+#print("Python version: " + var)
+
+"""print("Tell me anything...")
+anything = input()
+print("Hmm...", anything, "... Really?")"""
+
+"""anything = input("Tell me anything...")
+print("Hmm...", anything, "...Really?")"""
+
+
+
+
+"""x = int(input())
+y = int(input())
+x = x% y
+x =x % y
+y = y% x
+print(y)
+
+print(hello , world!)
+
+def fun(x):
+    if x % 2 == 0:
+        return 1
+    else:
+        return 2
+print(fun(fun(2)))
+
+t = (1,2,4,8)
+t = t[-2:-1]
+t=t[-1]
+print(t)
+
+d = {"1" : "0","0": "1"}
+for x in d.vals():
+    print(x,end="")
+
+
+def fun1:
+    return none
+def fun2:
+    return fun1 * fun1
+print(fun2(2))
+
+nums = [1,2,3]
+vals = nums
+del vals[:]"""
+
+
+print(1 // 2)
+
+
+
+
+
+
+
